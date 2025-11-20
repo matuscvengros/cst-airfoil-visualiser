ae02fe03cbf13807accdd62d448dcad08e6cb4bb
Matus Cvengros <matus.cvengros@gmail.com>
2025-11-21 10:30:26 +1100

    Deploy script correction for github pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react' // or whatever framework
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/cst-airfoil-visualiser/',
   plugins: [react(), tailwindcss()],
 })
